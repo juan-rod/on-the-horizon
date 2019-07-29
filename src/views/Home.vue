402379870e888b6894092245ca61400c28752cb1
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2019-07-29 08:52:13 -0500

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- <div class="home"> -->
+    <div class="main-page">
+      <div class="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sed error saepe veniam voluptatem neque, earum ullam numquam. Alias ad aspernatur natus, fugiat possimus similique reprehenderit unde voluptates sunt facere.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sed error saepe veniam voluptatem neque, earum ullam numquam. Alias ad aspernatur natus, fugiat possimus similique reprehenderit unde voluptates sunt facere.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sed error saepe veniam voluptatem neque, earum ullam numquam. Alias ad aspernatur natus, fugiat possimus similique reprehenderit unde voluptates sunt facere.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sed error saepe veniam voluptatem neque, earum ullam numquam. Alias ad aspernatur natus, fugiat possimus similique reprehenderit unde voluptates sunt facere.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sed error saepe veniam voluptatem neque, earum ullam numquam. Alias ad aspernatur natus, fugiat possimus similique reprehenderit unde voluptates iil
+      </div>
+    </div>
+  <!-- </div> -->
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+<style lang="scss" scoped>
+.main-page {
+  width: 100vw;
+  // border:1px solid orchid;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .text {
+    width: 300px;
+    text-align: justify;
+    font-size: .8em;
+  }
+}
+</style>
+
